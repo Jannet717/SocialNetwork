14011216dd2b5f0ab4472e4a48a5ff23e4fdd920
Jannet <jannetsayakbaeva@gmail.com>
2021-11-25 16:12:27 +0600

    init project

---
+++ main/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from account.models import MyUser
+
+
+class Post(models.Model):
+    author = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class PostImage(models.Model):
+    image = models.ImageField(upload_to='posts', blank=True, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
